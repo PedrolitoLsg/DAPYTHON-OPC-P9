2c66e0e0bcfece54b62502e3eebf1cf586a33855
PedrolitoLsg <84906663+PedrolitoLsg@users.noreply.github.com>
2021-12-13 16:58:27 +0100

    fixes 13/12 16h59

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -96,28 +96,6 @@ def flux(request):
     return render(request, 'flux.html', {'posts': posts})
 
 
-
-
-
-#####EXEMPLE FRAME
-def feed(request):
-    reviews = get_users_viewable_reviews(request.user)
-    # returns queryset of reviews
-    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
-    tickets = get_users_viewable_tickets(request.user)
-    # returns queryset of tickets
-    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
-    # combine and sort the two types of posts
-    posts = sorted(
-    chain(reviews, tickets),
-    key=lambda post: post.time_created,
-    reverse=True
-    )
-    return render(request, 'feed.html', context={'posts': posts})
-
-
-
-
 @login_required(login_url='app:connexion')
 def posts(request):
     tickets = Ticket.objects.filter(user=request.user)
